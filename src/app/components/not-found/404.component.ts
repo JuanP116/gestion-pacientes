b072cee6f272fb081e65154b8c979473a231ef15
JuanP116 <juanpferrete48@gmail.com>
2025-06-11 04:43:32 +0200

    añado el componente not-found

---
+++ src/app/components/not-found/404.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pagina-no-encontrada',
+  templateUrl: './404.component.html',
+  styleUrl: './404.component.css',
+  standalone: false
+})
+export class NotFoundComponent {
+
+}
